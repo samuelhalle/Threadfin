f28fe489d3b0b21725df697d59fe21702f75483a
Samuel Hallé <samuel.halle@okam.ca>
2023-11-14 14:50:18 +0000

    Sam personal changes

--- src/system.go
+++ src/system.go
@@ -353,7 +353,7 @@ func createStreamingURL(streamingType, playlistID, channelNumber, channelName, u
 		}
 	}
 
-	streamingURL = fmt.Sprintf("%s://%s/stream/%s", serverProtocol, System.Domain, streamInfo.URLid)
+	streamingURL = url
 	return
 }
 
